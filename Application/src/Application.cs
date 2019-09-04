3fae4851bab778e92831c6fa3733d7f372724970
Wessel van Zeggeren <wessel.van.zeggeren@gmail.com>
2019-09-04 16:08:48 +0200

    Fix in the git code (it somehow removed a bunch)

---
+++ Application/src/Application.cs
@@ -0,0 +1,26 @@
+﻿using Application.src.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.src
+{
+    class Application : IApplication
+    {
+
+        static void Main(string[] args)
+        {
+
+            BikeBluetooth bike = new BikeBluetooth(new Application());
+            bike.startConnection();
+        }
+
+        public void receiveProtocol(Protocol protocol)
+        {
+
+            Console.WriteLine(protocol.serviceName);
+        }
+    }
+}
