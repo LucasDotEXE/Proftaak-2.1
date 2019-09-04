da7d9894450839700fc6508eaebef1f7a5b6e670
Wessel <wesselvanz@gmail.com>
2019-09-04 10:46:46 +0200

    Basic application without functionality

---
+++ Application/src/model/BikeSimulation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.src.model
+{
+    class BikeSimulation : IBike
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void sendData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Application/src/model/Protocol.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.src.model
+{
+    class Protocol
+    {
+
+        // ToDo - add all the correct fields contained in the bytes!
+        public double speed
+        { get; }
+        public double heartrate
+        { get; }
+
+        public Protocol(string bytes)
+        {
+
+            // ToDo - add the decription of the bytes!
+        }
+    }
+}
