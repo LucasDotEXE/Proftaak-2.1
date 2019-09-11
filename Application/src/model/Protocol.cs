19c15b90bb10082308c63edcb401c395768b2970
Wessel <wesselvanz@gmail.com>
2019-09-11 10:13:22 +0200

    Changed code layout with additional bug fixes

--- Application/src/model/Protocol.cs
+++ Application/src/model/Protocol.cs
@@ -3,7 +3,6 @@
     class Protocol : observing.Observer<byte[]>
     {
 
-        // ToDo - add all the correct fields contained in the bytes!
         public string serviceName
         { get; set; }
         public double speed
@@ -17,28 +16,13 @@
         public int currentPower
         { get; set; }
 
-        public Protocol(string serviceName)
-        {
-            this.serviceName = serviceName;
-        }
-
         public Protocol(string serviceName, byte[] bytes)
         {
+
             this.serviceName = serviceName;
-            switch (bytes[4])
-            {
-                case 0x10:
-                    speed = bytes[9] + bytes[8];
-                    distance = bytes[7];
-                    break;
-                case 0x14: break;
-                case 0x19:
-                    acumilatedPower = bytes[8] + bytes[7];
-                    currentPower = bytes[10] + bytes[9];
-                    break;
-            }
-            // ToDo - add the decription of the bytes!
+            this.updateProtocol(bytes);
         }
+
         public void updateProtocol(byte[] bytes)
         {
             
@@ -54,16 +38,21 @@
                     currentPower = bytes[10] + bytes[9];
                     break;
             }
-
-            
-            // ToDo - add the decription of the bytes!
         }
 
         public override void update(byte[] content)
         {
+
             updateProtocol(content);
-            //System.Console.WriteLine($"Speed: {speed}, Distance: {distance}, acumPower: {acumilatedPower}, currPower: {currentPower}");
+        }
+
+        public override string ToString()
+        {
+
+            return string.Format(
+                "ServiceName: {0}\nSpeed: {1}\nHeartrate: {2}\nDistance: {3}\nAcumilatedPower: {4}\nCurrentPower: {5}\n",
+                 serviceName, speed, heartrate, distance, acumilatedPower, currentPower
+            );
         }
     }
-
 }
--- Application/src/model/Simulators.cs
+++ Application/src/model/Simulators.cs
@@ -4,170 +4,185 @@ using System.Collections.Generic;
 using System.Text;
 using System.Timers;
 
-
-class testSim : Simulator
+namespace Application.src.model 
 {
-    protected override byte[] generateData()
+
+    class testSim : Simulator
     {
-        return Encoding.UTF8.GetBytes("it works");
-    }
-}
-
-class BikeSimulatorPower : Simulator
-{
-    protected override byte[] generateData()
-    {
-        byte[] array = new byte[11];
-
-        array[4] = 0x19;
-
-        array[7] = 4;  //acumilatedPower
-        array[8] = 4; 
-
-        array[9] = 3; // currentPower
-        array[10] = 5;
-
-
-        return array;
-    }
-
-    private String dataToString()
-    {
-        throw new NotImplementedException();
-    }
-}
-class BikeSimulatorSpeed : Simulator
-{
-    protected override byte[] generateData()
-    {
-        byte[] array = new byte[11];
-        array[4] = 0x10; // Identifier
-
-        if (base.simulationData.indexer >= base.simulationData.effortCurve.Count)
+    
+        protected override byte[] generateData()
         {
-            base.simulationData.indexer = 0;
+            return Encoding.UTF8.GetBytes("it works");
         }
-        byte[] speed = BitConverter.GetBytes(base.simulationData.effortCurve[base.simulationData.indexer] * 10 + 100);
-        base.simulationData.indexer++;
-
-        array[8] = speed[0]; //will be added to get speed
-        array[9] = speed[1];
-
-        array[7] = 0x24; //distance
-
-        return array;
     }
-}
 
-class HeartSimulator : Simulator
-{
-    protected override byte[] generateData()
+    class BikeSimulatorPower : Simulator
     {
-        byte HeartRateValueTypeIs16 = 1;
-        byte SensorContactSupportNetwork = 1;
-        byte SensorContactSupportLocal = 1;
-        byte EnergyExpandedIncluded = 1;
-        byte RRIntervalIncluded = 1;
-
-        if (base.simulationData.indexer >= base.simulationData.effortCurve.Count)
+        protected override byte[] generateData()
         {
-            base.simulationData.indexer = 0;
+            byte[] array = new byte[11];
+
+            array[4] = 0x19;
+
+            array[7] = 4;  //acumilatedPower
+            array[8] = 4; 
+
+            array[9] = 3; // currentPower
+            array[10] = 5;
+
+
+            return array;
         }
 
-
-        int BPM = base.simulationData.effortCurve[base.simulationData.indexer] * 10 + 90;
-        int EnergyExpanded = base.simulationData.effortCurve[base.simulationData.indexer] * 10 + 20;
-        base.simulationData.indexer++;
-
-        byte[] array = new byte[9] {
-            HeartRateValueTypeIs16,
-            SensorContactSupportNetwork,
-            SensorContactSupportLocal,
-            EnergyExpandedIncluded,
-            RRIntervalIncluded,
-            4,                  //BPM 1stbit
-            4,                  //BPM 2ndbit
-            3,                  //Expand 1stbit
-            7                   //Expand 2ndbit
-        };
-
-
-
-
-        return array; // kon geen anderen manier vinden om het naar string te krijgen
+        private String dataToString()
+        {
+            throw new NotImplementedException();
+        }
     }
-}
 
-class SimulationData
-{
-    public List<int> effortCurve { get;  } = new List<int> { 1, 1, 1, 2, 2, 3, 4 };
-    public int indexer { get; set; } = 0;
-
-    public SimulationData()
-    {     }
-}
-
-class Printer : Observer<byte[]>
-{
-    private String printerName;
-
-    public Printer(string printerName)
+    class BikeSimulatorSpeed : Simulator
     {
-        this.printerName = printerName;
+
+        protected override byte[] generateData()
+        {
+            byte[] array = new byte[11];
+            array[4] = 0x10; // Identifier
+
+            if (base.simulationData.indexer >= base.simulationData.effortCurve.Count)
+            {
+                base.simulationData.indexer = 0;
+            }
+            byte[] speed = BitConverter.GetBytes(base.simulationData.effortCurve[base.simulationData.indexer] * 10 + 100);
+            base.simulationData.indexer++;
+
+            array[8] = speed[0]; //will be added to get speed
+            array[9] = speed[1];
+
+            array[7] = 0x24; //distance
+
+            return array;
+        }
     }
 
-    public override void update(byte[] content)
+    class HeartSimulator : Simulator
     {
-        Console.WriteLine($"{printerName} prints: " + content);
+        protected override byte[] generateData()
+        {
+            byte HeartRateValueTypeIs16 = 1;
+            byte SensorContactSupportNetwork = 1;
+            byte SensorContactSupportLocal = 1;
+            byte EnergyExpandedIncluded = 1;
+            byte RRIntervalIncluded = 1;
+
+            if (base.simulationData.indexer >= base.simulationData.effortCurve.Count)
+            {
+                base.simulationData.indexer = 0;
+            }
+
+
+            int BPM = base.simulationData.effortCurve[base.simulationData.indexer] * 10 + 90;
+            int EnergyExpanded = base.simulationData.effortCurve[base.simulationData.indexer] * 10 + 20;
+            base.simulationData.indexer++;
+
+            byte[] array = new byte[9] {
+                HeartRateValueTypeIs16,
+                SensorContactSupportNetwork,
+                SensorContactSupportLocal,
+                EnergyExpandedIncluded,
+                RRIntervalIncluded,
+                4,                  //BPM 1stbit
+                4,                  //BPM 2ndbit
+                3,                  //Expand 1stbit
+                7                   //Expand 2ndbit
+            };
+
+
+
+
+            return array; // kon geen anderen manier vinden om het naar string te krijgen
+        }
+    }
+
+    class SimulationData
+    {
+        public List<int> effortCurve { get;  } = new List<int> { 1, 1, 1, 2, 2, 3, 4 };
+        public int indexer { get; set; } = 0;
+
+        public SimulationData()
+        {     }
+    }
+
+    class Printer : Observer<byte[]>
+    {
+    
+        private String printerName;
+
+        public Printer(string printerName)
+        {
+            this.printerName = printerName;
+        }
+
+        public override void update(byte[] content)
+        {
+            Console.WriteLine($"{printerName} prints: " + content);
+        }
+    }
+
+    abstract class Simulator : Subject<byte[]>
+    {
+    
+        private Timer timer;
+        abstract protected byte[] generateData();
+        protected SimulationData simulationData { get; } = new SimulationData();
+
+        public void turnOn(int sec)
+        {
+
+            timer = new System.Timers.Timer(sec * 1000);
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e) => base.update(generateData());
     }
 }
 
-abstract class Simulator : Subject<byte[]>
-{
-    private Timer timer;
-    abstract protected byte[] generateData();
-    protected SimulationData simulationData { get; } = new SimulationData();
-
-    public void turnOn(int sec)
-    {
-        timer = new System.Timers.Timer(sec * 1000);
-        timer.Elapsed += OnTimedEvent;
-        timer.AutoReset = true;
-        timer.Enabled = true;
-    }
-
-    private void OnTimedEvent(object sender, ElapsedEventArgs e) => base.update(generateData());
-
-
-}
 
 
 namespace observing
 {
+    
     abstract class Observer<T>
     {
+
         abstract public void update(T content);
     }
 
     abstract class Subject<T>
     {
+        
         List<Observer<T>> observers;
 
         protected Subject()
         {
+            
             observers = new List<Observer<T>>();
         }
 
         public void update(T content)
         {
+            
             foreach (Observer<T> observer in observers)
             {
+            
                 observer.update(content);
             }
         }
 
         public void subscibe(Observer<T> observer)
         {
+
             observers.Add(observer);
         }
     }
