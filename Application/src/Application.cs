2bcc4bf078c65eac23124d344a2c0bd4fea6f04a
Tjalling <tjallingts@gmail.com>
2019-09-12 15:02:32 +0200

    added a method to create the tunnel
    -TODO: make an ID selector for deciding which session to join.

--- Application/src/Application.cs
+++ Application/src/Application.cs
@@ -6,14 +6,14 @@ namespace Application.src
     class Application : IApplication
     {
 
-        static void Main(string[] args)
+       /* static void Main(string[] args)
         {
 
             IBike bike = new BikeSimulation(new Application());
             bike.startConnection();
             
             Console.Read();
-        }
+        }*/
 
         public void receiveProtocol(Protocol protocol)
         {
--- Application/src/VR_Client.cs
+++ Application/src/VR_Client.cs
@@ -11,34 +11,31 @@ namespace VR_Client
     {
         static void Main(string[] args)
         {
-            string a = "";
-            JObject o;
+            
             //client object to build client
             Client client = new Client();
-            //if decoding fails retry
-        retry:
-            //sends data
-            client.send(new { id = "session/list" });
-            // calls receive method to get server response
-            a = client.receive();
-            //create json object from the message 
-            try
-            {
-                o = JObject.Parse(a);
-            }
-            catch
-            {
-                goto retry;
-            }
-            JArray data = (JArray)o["data"];
-            List<JObject> sessionList = data.ToObject<List<JObject>>();
-            //close all connections
-            client.close();
+            //gets session list
+            List<JObject> sessions = client.getSessionList();
+          
             //print sessions
-            foreach (JObject session in sessionList)
+            foreach (JObject session in sessions)
             {
                 Console.WriteLine(session["id"].ToString());
             }
+            Console.WriteLine("connecting to 15ce6013-9345-48ff-88aa-46ebbbaa4c0f");
+            client.createTunnel(new
+            {
+                id = "tunnel/create",
+                data = new
+                {
+                session = "4845aabc-bcfa-4b3f-8d84-a954f2634257",
+                key = ""
+                }
+            });
+
+            //close all connections
+            client.close()
+                ;
             //wait for input
             Console.Read();
         }
@@ -62,6 +59,37 @@ namespace VR_Client
             dStream = client.GetStream();
             Console.WriteLine($"client connected? = { client.Connected}");
         }
+        public List<JObject> getSessionList()
+        {
+            JObject ReceivedJsonObject;
+            String receivedJsonString;
+        retry:
+            //sends data
+            send(new { id = "session/list" });
+            // calls receive method to get server response
+            receivedJsonString = receive();
+            //create json object from the message 
+            try
+            {
+                ReceivedJsonObject = JObject.Parse(receivedJsonString);
+            }
+            catch
+            {
+                goto retry;
+            }
+            JArray data = (JArray)ReceivedJsonObject["data"];
+            List<JObject> sessionList = data.ToObject<List<JObject>>();
+            //close all connections
+            return sessionList;
+        }
+        public void createTunnel(dynamic pin)
+        {
+            send(pin);
+            string data = receive();
+            JObject Jdata = JObject.Parse(data);
+            string SessionID = Jdata["data"]["id"].ToObject<string>();
+            Console.WriteLine(Jdata["data"]["status"]);
+        }
 
         public void send(dynamic msg)
         {
@@ -71,6 +99,7 @@ namespace VR_Client
             byte[] length = BitConverter.GetBytes(packet.Length);
             dStream.Write(length, 0, length.Length);
             dStream.Write(packet, 0, packet.Length);
+            Console.WriteLine($"sending : {json}");
             dStream.Flush();
         }
 
