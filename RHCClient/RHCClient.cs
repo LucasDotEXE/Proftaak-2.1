8078cd15f2b0d90c4181c75824f463df614f6c7a
Noah <noah.van.ommen@hotmail.com>
2019-09-25 12:20:26 +0200

    Login + Message for Client Client
    client

--- RHCClient/RHCClient.cs
+++ RHCClient/RHCClient.cs
@@ -5,9 +5,44 @@ namespace RHCClient
 {
     class RHCClient : SocketClient
     {     
-        public void sendClientMessage(string message)
+        public void sendClientMessage(string MessageType)
         {
-            
+            switch (MessageType)
+            {
+                case Config.loginPreset:
+                    bool wronganswer = true;
+                    bool newuser = true;
+                    Console.WriteLine("Enter your username: ");
+                    string username = Console.ReadLine();
+                    Console.WriteLine("Enter your password: ");
+                    string password = Console.ReadLine();
+                    while (wronganswer) {
+                    Console.WriteLine("Are you new to this application? y/n");
+                    string answer = Console.ReadLine();
+                        if (answer.Equals("y"))
+                        {
+                            newuser = true;
+                            wronganswer = false;
+                        }
+                        if (answer.Equals("n"))
+                        {
+                            newuser = false;
+                            wronganswer = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter y/n");
+                        }
+                    
+                    }
+                    string loginMessage = String.Format("{0}:{1}:{2}",username, password, newuser);
+                    sendMessage(Config.loginPreset + loginMessage);
+                    break;
+                case Config.messagePreset:
+                    string mMessage = Console.ReadLine();
+                    sendMessage(Config.messagePreset + mMessage);
+                    break;
+            }
         }
     }
 }
