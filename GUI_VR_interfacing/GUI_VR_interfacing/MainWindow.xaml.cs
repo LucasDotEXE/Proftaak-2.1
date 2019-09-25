e3e474c75fef3b47a9bc11def6231127ead8a934
Tjalling <tjallingts@gmail.com>
2019-09-25 10:20:56 +0200

    Revert "Rewritten client to make it read constantly"
    This reverts commit 500b98167456bc7dcb05e3275487252584888084.

--- GUI_VR_interfacing/GUI_VR_interfacing/MainWindow.xaml.cs
+++ GUI_VR_interfacing/GUI_VR_interfacing/MainWindow.xaml.cs
@@ -12,10 +12,7 @@ namespace GUI_VR_interfacing
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static Client client = new Client();
-
-        internal static Client Client { get => client; }
-
+        Client client = new Client();
         public MainWindow()
         {
             InitializeComponent();
@@ -26,8 +23,7 @@ namespace GUI_VR_interfacing
         {
             if (SessionComboBox.SelectedItem != null)
             {
-                Data d = (Data)SessionComboBox.SelectedItem;
-                Client.createTunnel(d.id);
+                client.createTunnel(selected);
                 Console.WriteLine("Connected!");
             }
             TextBlock.Text = "Please select a Session first!";
@@ -41,17 +37,13 @@ namespace GUI_VR_interfacing
 
         private void refresh()
         {
-            List<JObject> l = Client.getSessionList();
-            List<Data> sessions = new List<Data>();
-            foreach (JObject o in l)
-            {
-                sessions.Add(new Data(o["id"].ToString(), o["clientinfo"]["user"].ToString()));
-            }
-            SessionComboBox.ItemsSource = sessions;
+            client.AskSessionList();
+            
+            SessionComboBox.ItemsSource = client.sessions;
             SessionComboBox.DisplayMemberPath = "id";
             DG_sessions.ItemsSource = sessions;
         }
 
     }
 }
-
+}
