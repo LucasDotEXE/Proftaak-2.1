ec68fed9dda280cb02a7cc47033984e0c21f4337
Tjalling <tjallingts@gmail.com>
2019-10-09 12:58:10 +0200

    added RHVR

---
+++ RH/RHVR/VRHelpers/VrEnvirement.cs
@@ -0,0 +1,96 @@
+﻿using CommandHelperObjects;
+using System.Collections.Generic;
+
+namespace GUI_VR_interfacing
+{
+    static class VREnviorment
+    {
+
+        public static void init(Client vrClient)
+        {
+            addTerain(vrClient, 1024, 1024);
+            //Commands.Scene.get(); //see if the groundpane is among it 
+            changeTime(vrClient, 0);
+            // changeTerrain(vrClient, heightMap.getSlopedHightMap(256, 256, 10), 256, 256);
+            add3DModdel(vrClient, "boom", new
+            {
+                model = new
+                {
+                    file = VRstandards.GenerateModelPaths()["City"]
+                },
+                transform = new
+                {   
+                    scale = 0.005
+                }
+            },
+            null
+            ) ;
+ 
+
+            // addRoute(vrClient, RouteNode.genRouteNodeList());
+
+            Commands.Route.show(true);
+        }
+
+        public static void addTerain(Client vrClient, int x, int y)
+        {
+            vrClient.AddToQueue(
+                            Commands.Scene.terrain.add(
+                                x, y, heightMap.getBlankHightMap(x, y)
+                                ));
+        }
+        public static void deleteNode(Client vrClient, string name)
+        {
+            vrClient.AddToQueue(Commands.Scene.node.delete(name));
+        }
+
+        public static void findNode(Client vrClient, string name)
+        {
+            vrClient.AddToQueue(Commands.Scene.node.find(name));
+        }
+
+        public static void changeTime(Client vrClient, float time)
+        {
+            vrClient.AddToQueue(
+                Commands.Scene.skybox.setTime(time)
+                );
+        }
+
+        public static void changeTerrain(Client vrClient, int[] heightMap, int x, int y)
+        {
+            vrClient.AddToQueue(
+                Commands.Scene.terrain.delete()
+                );
+
+            vrClient.AddToQueue(
+                Commands.Scene.terrain.add(x, y, heightMap)
+                );
+        }
+
+        public static void add3DModdel(Client vrClient, string name, dynamic component, string parent)
+        {
+            vrClient.AddToQueue(Commands.Scene.node.add(
+                name, component, parent
+                ));
+        }
+
+        public static void addRoute(Client vrClient, List<RouteNode> routeNodes)
+        {
+            vrClient.AddToQueue(
+                Commands.Route.add(
+                    routeNodes
+                    ));
+        }
+
+        public static void showRoute(Client vrclient, bool doShow)
+        {
+            vrclient.AddToQueue(
+                Commands.Route.show(doShow)
+                );
+        }
+        public static void getScene(Client client)
+        {
+            client.AddToQueue(Commands.Scene.get());
+        }
+    }
+}
---
+++ RH/RHVR/Windows/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GUI_VR_interfacing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Client client = new Client();
+        public MainWindow()
+        {
+            InitializeComponent();
+            SessionComboBox.ItemsSource = client.sessions;
+            SessionComboBox.DisplayMemberPath = "user";
+            DG_sessions.ItemsSource = client.sessions;
+            refresh();
+        }
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (SessionComboBox.SelectedItem != null)
+            {
+                string d = JToken.Parse(JsonConvert.SerializeObject(SessionComboBox.SelectedItem))["id"].ToString();
+                client.CreateTunnel(d);
+                Console.WriteLine("Connected!");
+                ControlPanel control = new ControlPanel();
+                control.DataContext = this;
+                control.start(client);
+                control.Show();
+                Close();
+            }
+            TextBlock.Text = "Please select a Session first!";
+        }
+
+        private void SelectSessionHandler(object sender, SelectionChangedEventArgs e)
+        {
+            string d = JToken.Parse(JsonConvert.SerializeObject(SessionComboBox.SelectedItem))["id"].ToString();
+            TextBlock.Text = "ID : " + d;
+        }
+
+        private void refresh()
+        {
+            client.AskForSessionList();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            refresh();
+        }
+    }
+}
