d9f99436132ad9411ca1dc1ba9102c1e2ad5f009
Noah <noah.van.ommen@hotmail.com>
2019-10-09 12:46:20 +0200

    MY FINAL MESSAGE GOODBYE
    =)

--- GUI_VR_interfacing/GUI_VR_interfacing/ControlPanel.xaml.cs
+++ GUI_VR_interfacing/GUI_VR_interfacing/ControlPanel.xaml.cs
@@ -54,7 +54,7 @@ namespace GUI_VR_interfacing
         {
             dynamic dSelect = ComboBox_SelectNode.SelectedItem;
             JToken selected = JToken.Parse(JsonConvert.SerializeObject(dSelect));
-            VREnviorment.deleteNode(_client, selected["uuid"].ToString());
+            VREnviorment.findNode(_client, selected["name"].ToString());
         }
 
         private void Button_DeleteClick(object sender, RoutedEventArgs e)
